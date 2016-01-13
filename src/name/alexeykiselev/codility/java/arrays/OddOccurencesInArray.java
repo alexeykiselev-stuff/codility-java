69080d2976cda5c68902120284128a27041da277
kiselev@bitrete.ru <kiselev@bitrete.ru>
2016-01-13 22:49:46 +0300

    Solution of OddOccurencesInArray was implemented with XOR. Time spent 88 minutes.

--- src/name/alexeykiselev/codility/java/arrays/OddOccurencesInArray.java
+++ src/name/alexeykiselev/codility/java/arrays/OddOccurencesInArray.java
@@ -1,22 +1,12 @@
 package name.alexeykiselev.codility.java.arrays;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class OddOccurencesInArray {
 	public static int solution(int[] A) {
 		int result = 0;
-		Set<Integer> seen = new HashSet<>();
 		for (int i = 0; i < A.length; i++) {
-			if (seen.contains(A[i])) {
-				result -= A[i];
-				seen.remove(A[i]);
-			} else {
-				result += A[i];
-				seen.add(A[i]);
-			}
+			result = result ^ A[i];
 		}
-		
+
 		return result;
 	}
 }
